c270d9632a8e1c3e0a1758d4ca3de094672177f5
Ville Matti Rantala <VilleMattiRantala@gmail.com>
2017-10-03 15:38:19 +0300

    Add some goroutines

--- main.go
+++ main.go
@@ -3,44 +3,46 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"math/rand"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	t := start
 
 	w := flag.Int("w", 2048, "width")
 	h := flag.Int("h", 1024, "height")
 	r := flag.Int("r", 250, "region count")
+	n := flag.Int("n", 1, "number of worlds to generate")
 	s := flag.Int64("s", start.Unix(), "seed")
-	f := flag.String(
-		"f", fmt.Sprintf("img/%v.png", start.Unix()),
-		"output file path, supported formats .png, .jpg/.jpeg, .gif",
-	)
+	p := flag.String("p", fmt.Sprintf("img/%v", start.Unix()), "output file path")
+	f := flag.String("f", "png", "output format: png, jpg/jpeg, gif")
 	flag.Parse()
 
 	rand.Seed(*s)
 
-	t = time.Now()
-	fmt.Printf("Initializing...         ")
-	world := newWorld(*w, *h, *r)
-	fmt.Printf("%v\n", time.Now().Sub(t))
+	worlds := make(chan *world)
+	done := make(chan bool)
 
-	t = time.Now()
-	fmt.Printf("Filling gaps...         ")
-	world.expandRegions(-1, -1)
-	fmt.Printf("%v\n", time.Now().Sub(t))
-
-	t = time.Now()
-	fmt.Printf("Saving...               ")
-	err := world.image().save(*f)
-	if err != nil {
-		log.Fatal(err)
+	go func() {
+		for i := 0; i < *n; i++ {
+			world := <-worlds
+			t := time.Now()
+			world.image().save(fmt.Sprintf("%v-%v.%v", *p, i, *f))
+			fmt.Printf("Saved world, took 		%v\n", time.Now().Sub(t))
+		}
+		done <- true
+	}()
+	for i := 0; i < *n; i++ {
+		//go func(i int) {
+		t := time.Now()
+		world := newWorld(*w, *h, *r)
+		world.expandRegions(-1, -1)
+		worlds <- world
+		fmt.Printf("Created world %v, took 		%v\n", i, time.Now().Sub(t))
+		//}(i)
 	}
-	fmt.Printf("%v\n", time.Now().Sub(t))
 
-	fmt.Printf("Total time elapsed:     %v\n", time.Now().Sub(start))
+	<-done
+	fmt.Printf("Total time elapsed:		%v\n", time.Now().Sub(start))
 }
